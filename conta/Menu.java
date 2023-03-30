de4ab96625ebf41acc7444cd05bd64926ca936ac
Dario-alabakiz <127209518+Dario-alabakiz@users.noreply.github.com>
2023-03-30 20:36:51 -0300

    Update Menu.java

--- conta/Menu.java
+++ conta/Menu.java
@@ -21,27 +21,6 @@ public class Menu {
 		String titular;
 		float saldo = 0, limite, valor;
 
-		/*Conta c1 = new Conta(1, 123, 1, "Dario", 10000.0f);
-		c1.visualizar();
-		c1.sacar(12000.0f);
-		c1.visualizar();
-		c1.depositar(5000.0f);
-		c1.visualizar();
-
-		ContaCorrente cc1 = new ContaCorrente(1, 123, 1, "José da Silva", 15000.0f, 1000.0f);
-		cc1.visualizar();
-		cc1.sacar(12000.0f);
-		cc1.visualizar();
-		cc1.depositar(5000.0f);
-		cc1.visualizar();
-
-		ContaPoupança cp1 = new ContaPoupança(2, 123, 2, "Maria dos Santos", 100000.0f,15);
-		cp1.visualizar();
-		cp1.sacar(1000.0f);
-		cp1.visualizar();
-		cp1.depositar(5000.0f);
-		cp1.visualizar();
-*/
 		do {
 
 			System.out.println(Cores.TEXT_PURPLE + Cores.ANSI_BLACK_BACKGROUND);
